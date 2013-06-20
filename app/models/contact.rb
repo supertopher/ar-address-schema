334e487d70a2ed70048c97e91edcfe081d682e7a
supertopher <asimplechris@gmail.com>
2013-06-19 17:44:13 -0700

    Refactored phone number regex

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -8,7 +8,7 @@ class Contact < ActiveRecord::Base
   validates :email, :format => {  :with => /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/,
                                   :message => "Please enter a valid email address."}
   validates :email, :uniqueness => true
-  validates :phone, :format => {  :with => /[2-9]{2}\d{8}/,
+  validates :phone, :format => {  :with => /\d{3}.\d{3}.\d{4}.*\d*/,
                                   :message => "Please enter a valid 10-digit phone number."}
 
   def name
